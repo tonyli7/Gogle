d80ba1332c106234feaeb9384ff5a153b0014986
tonyli7 <xxtonyxxlixx@gmail.com>
2015-11-20 10:10:38 -0500

    dictionary

--- gogle.py
+++ gogle.py
@@ -3,6 +3,22 @@ import urllib2, google, bs4, re
 
 ["who","when"]
 q=input();
+
+def who(text):
+    namedict={}
+    exp = "[A-Z][a-z]+ [A-Z][a-z]+"
+    result = re.findall(exp,text);
+    for name in result:
+        if name not in namedict:
+            namedict[name]=0;
+        namedict[name]+=1;
+
+    test=[]
+    for something in namedict:
+        test+=[namedict[something]]
+    
+    return test.sort()
+
 def search(query):
     r = google.search(query,num=10,start=0,stop=10)
     l=[]
@@ -18,8 +34,9 @@ def search(query):
     raw = soup.get_text()
     #print raw
     text = re.sub("[\t\n ]"," ",raw)
-    exp = "[A-Z][a-z]+ [A-Z][a-z]+"
-    result = re.findall(exp,text);
-    return result
+    
+    return who(text)
 
+
+    
 print search(q)
